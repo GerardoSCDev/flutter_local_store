54c55f58bf24bdbdd6ff6d8a0024dffd3fae5335
Gerardo SC <gerardo.sc.dev@gmail.com>
2024-03-25 12:39:37 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/local_store/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.local_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:local_store/app/MyApp.dart';
+
+void main() {
+  runApp(const MyApp());
+}
